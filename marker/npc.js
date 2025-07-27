aacc4de89aad4ecec4b55ce9975d61f84a465613
Дмитрий <154452992+sh1ndoo@users.noreply.github.com>
2025-07-27 20:44:09 +0700

    Update npc.js

--- marker/npc.js
+++ marker/npc.js
@@ -5,13 +5,13 @@ var npc = {
             "type": "Feature",
             "properties": {
                 "id": "01",
-                "description": "."
+                "description": "Use pray action in the sack of blue shiny thing"
             },
             "geometry": {
                 "type": "Point",
                 "coordinates": [
-                    76.75,
-                    -186.25
+                    96.375,
+                    -13
                 ]
             }
         },
@@ -19,13 +19,13 @@ var npc = {
             "type": "Feature",
             "properties": {
                 "id": "02",
-                "description": "."
+                "description": "Do \"warm up\" action here in front of Ram"
             },
             "geometry": {
                 "type": "Point",
                 "coordinates": [
-                    56,
-                    -208
+                    78.625,
+                    -54.5
                 ]
             }
         },
@@ -33,13 +33,69 @@ var npc = {
             "type": "Feature",
             "properties": {
                 "id": "03",
-                "description": "."
+                "description": "In here you need to do \"Hi\" action"
             },
             "geometry": {
                 "type": "Point",
                 "coordinates": [
-                    104.5,
-                    -195
+                    43.25,
+                    -34.375
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "description": "Here you need to do the action \"Support\". You will get this from Island expedition once you occupied an island.",
+                "id": "04"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    34.625,
+                    -41.625
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "05",
+                "description": "Press that interact and look for sweep floor. For you to be able to get that, you need to look for 30 feline and jar in Sage forest."
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    30.5,
+                    -34.125
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "06",
+                "description": "Talk to this cat named Hill at 19:00-19:59 for you to get 1000 light gems."
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    55.125,
+                    -72.125
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "07",
+                "description": "At exactly 12:00 noon, Talk to Aye"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    27.625,
+                    -65.75
                 ]
             }
         }
